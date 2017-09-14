4989af8eef4a6d03af317fa021e652e52d0a340c
Joegu17 <joel@familiehari.ch>
2017-09-14 13:59:51 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+
+
+var game = {
+    
+    init: function() {
+        
+        var w = window.innerWidth,
+            h = window.innerHeight;
+        
+        $('#hintergrund').css({width: w+'px', height: h+'px'});
+        $('#flugzeug').css({width: 215+'px', height: 67+'px'});
+        
+    }
+    
+}
